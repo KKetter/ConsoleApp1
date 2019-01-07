c62873b558adfb66c35cbb773e22d0be0f0a18fd
Kent Ketter <kent.ketter1@gmail.com>
2019-01-07 13:23:47 -0800

    readme updated

---
+++ ConsoleApp1/Assets/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿Function:
+1) Intake two numbers from user
+2) Intake operand (-, +, /, %)
+3) Output result to console
+
+Output Image:
+\ConsoleApp1\Assets\calc_output.PNG
+
+Shortcomings:
+Completed the exercise using a switch w/ help from syntax online but to meet the requirements for 4 methods I need to wrap my switch in a method.
+Then the switch can contain the needed methods (Add, Sub, Mult, Divi)
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,39 +10,39 @@ namespace Calculator
             int num1;
             int num2;
             string operand;
-            double answer = 0;
+            int answer = 0;
 
             Console.Write("Please enter the first integer: ");
             num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Please enter an operand (+, -, /, *): ");
-            operand = Console.ReadLine();
-
             Console.Write("Please enter the second integer: ");
             num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter an operand (+, -, /, *): ");
+            operand = Console.ReadLine();
 
             switch (operand)
             {
                 case "-":
                     answer = num1 - num2;
+                    Console.WriteLine($"Result: {answer}");
                     break;
                 case "+":
                     answer = num1 + num2;
+                    Console.WriteLine($"Result: {answer}");
                     break;
                 case "/":
                     answer = num1 / num2;
+                    Console.WriteLine($"Result: {answer}");
                     break;
                 case "*":
                     answer = num1 * num2;
+                    Console.WriteLine($"Result: {answer}");
                     break;
 
+                
             }
-
-            Console.WriteLine($"Result: {answer}");
-
                 Console.ReadLine();
-
-
 
         }
     }
